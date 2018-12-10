e7635662d356b1ba8a4580121f087f5c1947afef
iski13 <32782549+iski13@users.noreply.github.com>
2018-12-10 22:12:51 +0100

    Poprawienie błędu z przypisaniem rodzaju zlecenia.

--- Task.py
+++ Task.py
@@ -4,16 +4,16 @@ class Task():
 
     def __init__(self, start_timeH=18, start_timeM=0, tasktype = 1, dest = 0):            #Inicjalizacja zadania
         self.start_time = datetime.time(int(start_timeH), int(start_timeM))
-        self.tasktype = tasktype
+        self.tasktype = int(tasktype)
         self.dest = Place.Place(dest)
 
     def __init__(self, data):                                                             #Inicjalizacja zadania przez listę danych
         self.start_time = datetime.time(int(data[0]), int(data[1]))
-        self.tasktype = data[2]
+        self.tasktype = int(data[2])
         self.dest = Place.Place(data[3])
 
     def show(self):                                                                 #Prezentacja zadania przez print
-        if self.tasktype==1:
+        if self.tasktype == 1:
             return print("Dostarczenie na\t",self.start_time,"\tz\t",self.dest.name)
         else:
             return print("Odebranie o\t\t",self.start_time,"\tz\t",self.dest.name)
